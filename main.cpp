b3190ffa49d768b7034582596c7ed4e01a965ca3
Kuba <jakub.ryzewski@gmail.com>
2026-05-07 17:38:52 +0200

    Improved HashTable and Entry structs: added proper constructors. Added insert() method to the HashTable struct.

--- main.cpp
+++ main.cpp
@@ -1,36 +1,61 @@
 #include <iostream>
 #include <string>
 
-constexpr int TABLE_SIZE = 10;
+constexpr int TABLE_CAPACITY = 10;
 
-struct Entry{
-    std::string value;
-};
-
-struct HashTable{
-    int size;
-    Entry* table[TABLE_SIZE];
-};
-
-// ===================
-
+// ===== HASHING FUNCTIONS =====
 unsigned int hashFunction1(std::string value){
     unsigned int hash_value = 0;
     for(int i=0; i<value.length(); ++i){
         hash_value += value[i];
         hash_value *= value[i] - i;
     }
-    return hash_value % TABLE_SIZE;
+    return hash_value % TABLE_CAPACITY;
 }
 
+// szablon ponieważ będą 3 różne funkcje haszowania
 template <typename HashFunction>
-int hash(std::string value, HashFunction H){
+unsigned int hash(std::string value, HashFunction H){
     // TODO
     return 0;
 }
 
+// ======== STRUCTS ========
+struct Entry{
+    std::string value;
+    Entry* next;
+
+    Entry(std::string s){
+        value = s;
+        next = nullptr;
+    }
+};
+
+struct HashTable{
+    int size;
+    Entry* table[TABLE_CAPACITY];
+
+    HashTable() {
+        size = 0;
+        for(int i=0; i < TABLE_CAPACITY; ++i){
+            table[i] = nullptr;
+        }
+    }
+
+    void insert(std::string s){
+        unsigned int index = hash(s, hashFunction1);
+        Entry* new_entry = new Entry(s);
+        
+        new_entry->next = table[index];
+        table[index] = new_entry;
+    }
+
+    void remove(std::string s){
+
+    }
+
+};
 
 int main(){
-    std::string s = "Jablko";
-    std::cout<<s<<" --> "<<hashFunction1(s);
+    return 0;
 }
